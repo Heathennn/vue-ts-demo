70fb10916fee38ce726673cb96499128f0dfb370
Gnak <523748995@qq.com>
2019-08-23 17:30:00 +0800

    ts + vue 尝试

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page{{msg}}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+export default class About extends Vue {
+}
+</script>
